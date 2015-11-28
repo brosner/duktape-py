bbeb402a688cb7becf087f16775649e5744a4886
Brian Rosner <brian@brosner.com>
2015-11-28 11:45:40 -0700

    fixed setup.py styling

--- setup.py
+++ setup.py
@@ -1,21 +1,21 @@
-import sys
-from distutils.core import setup,Extension
+from distutils.core import setup, Extension
 
-duk_c=Extension(
-  'duktape',
-  sources=[
-    'duktape.c',
-    'duktape_c/duktape.c',
-  ]
-)
 
 setup(
-  name='duktape',
-  version='0.0.3',
-  description='python wrapper for duktape, an embeddable javascript library',
-  ext_modules=[duk_c],
-  author='Abe Winter',
-  author_email='abe-winter@users.noreply.github.com',
-  url='https://github.com/abe-winter/duktape-py',
-  keywords=['javascript','duktape'],
+    name="duktape",
+    version="0.0.3",
+    description="python wrapper for duktape, an embeddable javascript library",
+    ext_modules=[
+        Extension(
+            "duktape",
+            sources=[
+                "duktape.c",
+                "duktape_c/duktape.c",
+            ]
+        )
+    ],
+    author="Abe Winter",
+    author_email="abe-winter@users.noreply.github.com",
+    url="https://github.com/abe-winter/duktape-py",
+    keywords=["javascript", "duktape"],
 )
