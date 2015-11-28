1f10db8a12fbaaee89a7de72e8a0f1d5b013b855
Brian Rosner <brian@brosner.com>
2015-11-28 08:29:25 -0700

    removed cython requirement for installation

--- setup.py
+++ setup.py
@@ -1,11 +1,10 @@
 import sys
 from distutils.core import setup,Extension
-from Cython.Build import cythonize
 
 duk_c=Extension(
   'duktape',
   sources=[
-    'duktape.pyx',
+    'duktape.c',
     'duktape_c/duktape.c',
   ]
 )
@@ -14,7 +13,7 @@ setup(
   name='duktape',
   version='0.0.3',
   description='python wrapper for duktape, an embeddable javascript library',
-  ext_modules=cythonize([duk_c]),
+  ext_modules=[duk_c],
   author='Abe Winter',
   author_email='abe-winter@users.noreply.github.com',
   url='https://github.com/abe-winter/duktape-py',
